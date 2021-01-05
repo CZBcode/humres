6cf5b253bccb98683d925d9cb08633f44b6f8774
Gogs <gogs@fake.local>
2021-01-05 20:45:18 +0800

    init the springboot framework

--- src/main/java/auroraonce/humres/service/impl/loginServiceImpl.java
+++ src/main/java/auroraonce/humres/service/impl/loginServiceImpl.java
@@ -1,5 +1,11 @@
 package auroraonce.humres.service.impl;
 
-public class loginServiceImpl {
+import auroraonce.humres.service.loginService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class loginServiceImpl implements loginService {
+
+
 
 }
